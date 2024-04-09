bb32e89d40193ff8328252de196738d07282a736
Aina <ainaeinikyte@gmail.com>
2024-04-09 20:24:49 +0300

    feat: Auto list and Auto inner page

--- client/src/components/hero/Hero.jsx
+++ client/src/components/hero/Hero.jsx
@@ -2,7 +2,7 @@ import heroImg from '../../assets/bootstrap-themes.png'
 
 export function Hero() {
     return (
-<div className="container col-xxl-8 px-4 py-5">
+<div className="container col-xxl-8 px-4">
     <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
       <div className="col-10 col-sm-8 col-lg-6">
         <img src={heroImg} className="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy" />
