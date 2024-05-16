d805cb556bc1231c5ddff73ddebb40ed870c2fd5
Aina <ainaeinikyte@gmail.com>
2024-05-16 09:19:18 +0300

    feat: env

--- server/lib/hash.js
+++ server/lib/hash.js
@@ -1,9 +1,8 @@
 import { createHash } from "node:crypto";
-
-const salt = "548th7b5fv26e5resg41f5af2gs4";
+import { HASH_SALT_PASSWORD } from "../env";
 
 export function hash(str) {
   return createHash("sha256", { encoding: "utf8" })
-    .update(salt + str)
+    .update(HASH_SALT_PASSWORD + str)
     .digest("hex");
 }
